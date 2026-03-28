41292e27720e38cae53defccf462fcb8ef27877e
Janhvi-04 <janvi042005@gmail.com>
2026-03-28 22:03:07 +0530

    fix: add vercel config and update cors

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const { default: mongoose } = require('mongoose');
 dotenv.config();
 const app=express();
 const PORT=process.env.PORT || 5000;
-app.use(cors());
+app.use(cors({
+    origin: "https://secret-vault-frontend-one.vercel.app/",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 app.use(express.json());
 app.use('/api/auth',authRoutes);
 app.use('/api/secrets',secretRoutes)
